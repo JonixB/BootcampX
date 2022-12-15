cab041e5e3df186da38d883b78e544a4ec48d765
Nikko Bansil <bjohnnikko@gmail.com>
2022-12-14 19:00:21 -0800

    js that shows all teachers that assisted on a cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${process.argv[2]}%';
+`)
+  .then(res => {
+    for (const record of res.rows) {
+      console.log(`${record.cohort}: ${record.teacher}`);
+    }
+  })
+  .catch(err => console.error('query error', err.stack));
