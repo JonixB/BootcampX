e8b21fb3bd4c08c3570c9496bc6c07e537fbef9e
Nikko Bansil <bjohnnikko@gmail.com>
2022-12-14 19:45:41 -0800

    change to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [process.argv[2], process.argv[3]];
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE '%' || $1 || '%'
+LIMIT $2;
+`, values)
   .then(res => {
     for (const record of res.rows) {
       console.log(`${record.name} has an id of ${record.id} and was in the ${record.cohort_name} cohort`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,15 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const values = [process.argv[2]];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%';
-`)
+WHERE cohorts.name LIKE '%' || $1 || '%';
+`, values)
   .then(res => {
     for (const record of res.rows) {
       console.log(`${record.cohort}: ${record.teacher}`);
